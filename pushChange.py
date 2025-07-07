fe27e8968c71c475daefed63a8ad3a9dc31dde5d
Twind2 <sunzihang556@gmail.com>
2025-07-07 20:57:19 +0800

    Update pushChange.py

--- pushChange.py
+++ pushChange.py
@@ -1,15 +1,19 @@
 def push(result, content="",summary=""):
     import requests
     import secret
-
+    import os
     url = "https://wxpusher.zjiecode.com/api/send/message"
+    app_token = os.environ.get('APPTOKEN')
+    topic_ids_str = os.environ.get('TOPICIDS')
+    uids_str = os.environ.get('UIDS')
+    
     json_data = {
-        "appToken":secret.appToken,
+        "appToken":app_token,
         "content": content,
         "summary": summary, 
         "contentType":1,
-        "topicIds":secret.topicIds,
-        "uids":secret.uids,
+        "topicIds":topic_ids_str,
+        "uids":uids_str,
         "verifyPay":"false", 
         "verifyPayType":0 }
     if result:
