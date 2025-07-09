065751e2271a9012db9ac12b4f4f0a70e5031235
Twind2 <sunzihang556@gmail.com>
2025-07-09 14:24:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,61 @@
 from getscore import get_score
 import json
-import os
-from pushChange import push
 import pushChange
+import writefile
 date = get_score()
 course_list = []
+newTotalResult = date['totalResult']
+scoreChange = False  # 用于判断成绩是否有变化
+noneOldScore = False
+content = ""  # 用于存储成绩信息
 
-for item in date['items']:
-    course_info = {
-        '课程名称': item['kcmc'],
-        '成绩': item['cj'],
-        '任课老师': item['cjbdczr'],
-        '成绩提交时间': item['cjbdsj']
-    }
-    course_list.append(course_info)
-
-with open('grades_json_new.txt', 'w', encoding='utf-8') as f:
-    json.dump(course_list, f, ensure_ascii=False, indent=4)
-
-
-###
-old_filename = 'grades_json_old.txt'
-input_filename_new = 'grades_json_new.txt'
-output_filename = 'grades_json_old.txt'
-
-# 读取新的JSON文件内容 进行排序 但不保存
-try:
-    with open(input_filename_new, 'r', encoding='utf-8') as f:
-        grades_data = json.load(f)
-        if not grades_data:
-            pushChange.push(0)
-            os.remove(input_filename_new)  # 删除空文件
-            exit()
-    sorted_grades_new = sorted(grades_data, key=lambda item: item['成绩提交时间'], reverse=True)
-    content = sorted_grades_new[0]["课程名称"] + " 成绩：" + sorted_grades_new[0]["成绩"] + "，任课老师：" + sorted_grades_new[0]["任课老师"] + "，成绩提交时间：" + sorted_grades_new[0]["成绩提交时间"]
-except:
-    pushChange.push(0)
+if newTotalResult == 0:  # 如果没有成绩
+    pushChange.push(1, "老师还没有上传成绩，请耐心等待", "没有成绩")
     exit()
+else:
+    for item in date['items']:
+        course_info = {
+            'courseName': item['kcmc'],
+            'score': item['cj'],
+            'teacherName': item['cjbdczr'],
+            'newScoreSubmissionTime': item['cjbdsj']
+        }
+        course_list.append(course_info)
+# 将成绩列表进行排序
+sorted_grades_new = sorted(course_list, key=lambda item: item['newScoreSubmissionTime'], reverse=True)
+jsonContent = {"newScoreSubmissionTime": sorted_grades_new[0]["newScoreSubmissionTime"],
+               "TotalResult": newTotalResult}
 
 # 对旧的JSON文件进行读取 
 try:
-    with open(old_filename, 'r', encoding='utf-8') as f:
+    with open('grades_json_old.json', 'r', encoding='utf-8') as f:
         grades_data_old = json.load(f)
-        if not grades_data_old: #旧文件为空
-            pushChange.push(1,content,"有新成绩了！")
-            with open(output_filename, 'w', encoding='utf-8') as f:
-                json.dump(sorted_grades_new, f, ensure_ascii=False, indent=4)
-                os.remove(input_filename_new)  # 删除新文件
-                exit()
+except:  # 如果旧成绩文件不存在或无法读取，则认为没有旧成绩
+    with open('grades_json_old.json', 'w', encoding='utf-8') as f:
+        noneOldScore = True
+        grades_data_old = {
+            "newScoreSubmissionTime": "",
+            "TotalResult": 0
+        }
 
-except: #如果旧成绩文件不存在或无法读取，则认为没有旧成绩
-    with open(output_filename, 'w', encoding='utf-8') as f:
-        pushChange.push(1,content,"有新成绩了！")
-        json.dump(sorted_grades_new, f, ensure_ascii=False, indent=4)
-        os.remove(input_filename_new)
-        exit()
-        
-    
-if sorted_grades_new[0]["成绩提交时间"] <= grades_data_old[0]["成绩提交时间"]:
-        os.remove(input_filename_new)  # 删除新文件
-        pushChange.push(0,"成绩没有更新哦！","成绩没有更新哦！")
-        exit()
+changeNumber = jsonContent["TotalResult"] - grades_data_old["TotalResult"]
+
+# 对比新旧成绩的数目
+if noneOldScore:
+    scoreChange = True
+    for gradeInfo in sorted_grades_new[:jsonContent["TotalResult"]]:
+        content += gradeInfo["courseName"] + "成绩：" + gradeInfo["score"] + "，任课老师：" + gradeInfo["teacherName"] + "，成绩提交时间：" + gradeInfo["newScoreSubmissionTime"] +"\n"
+elif changeNumber > 0:  # 如果新成绩数目大于旧成绩数目
+    scoreChange = True
+    for gradeInfo in sorted_grades_new[:changeNumber]:
+        content += gradeInfo["courseName"] + "成绩：" + gradeInfo["score"] + "，任课老师：" + gradeInfo["teacherName"] + "，成绩提交时间：" + gradeInfo["newScoreSubmissionTime"] +"\n"
+elif changeNumber < 0:
+    pushChange.push(1, "请删除grades_json_old.json后重试！！", "出错了")
 else:
-    pushChange.push(1,content,"有新成绩了！")
-    os.remove(input_filename_new)
-    with open(output_filename, 'w', encoding='utf-8') as f:
-        json.dump(sorted_grades_new, f, ensure_ascii=False, indent=4)
+    scoreChange = False
+# 将新成绩更新时间，总数目写入旧成绩文件
+if scoreChange:
+    writefile.writeFile('grades_json_old.json', jsonContent)
+    pushChange.push(1, content, "有"+ str(changeNumber) +"门新成绩了！")
+print(content,scoreChange,changeNumber)
+
